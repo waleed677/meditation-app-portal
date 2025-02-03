9d1fe0d583b24013bcc3a10957a2cf3dddda8d0b
hassanmughalbaramdatsol <145018430+hassanmughalbaramdatsol@users.noreply.github.com>
2025-02-03 15:56:50 +0500

    check

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     cors: {
-      origin: "https://mintcream-cod-221842.hostingersite.com", // Your CORS origin for the frontend
+      origin: "https://meditation.baramdatsol.com", // Your CORS origin for the frontend
     },
     proxy: {
       "/api": {
-        target: "https://mintcream-cod-221842.hostingersite.com",
+        target: "https://meditation.baramdatsol.com",
         changeOrigin: true,
         secure: false, // Useful for local dev with self-signed certificates
       },
