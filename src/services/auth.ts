5d677ac015ee765f365a0e14afebc16532c78df3
waleed677 <waleed@shopnjoy.com>
2025-02-03 16:33:58 +0500

    ..

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "/auth",
+    baseUrl: "https://dummyjson.com/auth/",
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
       const token = localStorage.getItem("authToken");
