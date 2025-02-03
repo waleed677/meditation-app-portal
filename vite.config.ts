a0907042f635dfa52afc48eb4b0b5d3e6f0526f3
waleed677 <waleed@shopnjoy.com>
2025-02-03 14:45:05 +0500

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    cors: {
+      origin: "https://mintcream-cod-221842.hostingersite.com", // Your CORS origin for the frontend
+    },
     proxy: {
       "/api": {
         target: "https://mintcream-cod-221842.hostingersite.com",
